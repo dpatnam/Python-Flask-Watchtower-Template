00e1d3bfc6f8de7f4f3532268dd90764f5423839
dpatnam <64935399+dpatnam@users.noreply.github.com>
2020-05-20 13:37:54 -0700

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This is a skeleton Flask App for the WatchTower Application!"
+    return "This is a skeleton Flask App for the Incident Manager WatchTower Application!"
